6a404d4588a1aff1ee8a241d2abbd96ae86324bd
BebopVinh <vinhdo07@gmail.com>
2019-05-29 17:56:36 -0700

    Move update_buttons helper method to contribution_helper.rb

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -20,11 +20,6 @@ module ApplicationHelper
    end
 
 
-   def update_buttons(contribution)
-      if contribution.user == current_user
-         button_to "Edit Contribution", edit_contribution_path(contribution), method: :get, class: "pure-button pure-button-primary button-small"
-      end
-   end
 
    def auth_buttons
       if user_signed_in?
