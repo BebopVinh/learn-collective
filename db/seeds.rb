ecfe5fe50f4cf4d61b379b87d659829938f62be8
BebopVinh <vinhdo07@gmail.com>
2019-06-11 16:31:34 -0700

    Seed categories and sections

---
+++ db/seeds.rb
@@ -0,0 +1,38 @@
+# js_sections = [
+#    "Associations and Rails",
+#    "Basics",
+#    "Data Structures",
+#    "Looping and Iteration",
+#    "Advanced Functions",
+#    "Object-Oriented JS",
+#    "Advanced Scope",
+#    "DOM and Events",
+#    "Asynchronous JavaScript",
+#    "[Bonus] Advanced Testing"
+# ]
+
+# js_rails_sections = [
+#    "[Bonus] Workers",
+#    "Asset Pipeline",
+#    "Consuming APIs",
+#    "HTML Forms",
+#    "Building API",
+#    "Project Mode"
+# ]
+
+# categories = [
+#    "Rails",
+#    "JavaScript",
+#    "Rails and JavaScript"
+# ]
+
+# categories.each {|c| Category.create(name: c)}
+
+# js_sections.each do |s|
+#    Section.create(name: s, category_id: 2)
+# end
+
+# js_rails_sections.each do |s|
+#    Section.create(name: s, category_id: 3)
+# end
+
