96149f18652b34f26da2976c13f5ae2ec77ad535
BebopVinh <vinhdo07@gmail.com>
2019-06-12 08:45:28 -0700

    add click actions for category links and section links

--- app/assets/javascripts/categories.js
+++ app/assets/javascripts/categories.js
@@ -1,21 +1,53 @@
 $(document).on('ready page:load', function () {
-   console.log("ready!")
+   console.log("categories.js ready!")
    $("a.category").click(function (event) {
       event.preventDefault()
+      $("ul.section").remove()
       let id = $(this).data("id")
       let categoryChild = ""
       $.get(`/categories/${id}.json`, function (categories) {
          const sections = categories["sections"].map(section => section)
          sections.forEach(function (section) {
             categoryChild += `
-               \<div class="section" id="${section["id"]}" data-id="${section["id"]}">
-                  <p>${section["name"]}</p>\
-               </div>\
+               <div class="section" id="${section["id"]}" data-id="${section["id"]}">
+                  <a href="#" class="section" data-id="${section["id"]}">${section["name"]}</a>
+               </div>
             `
          })
-         $(`a.category#${id}`).append(`<ul>${categoryChild}</ul>`)
+         $(`div.category#${id}`).append(
+            `<ul class="section">
+               <strong>Sections</strong>
+               <br><br>
+               ${categoryChild}
+            </ul>`
+         )
       })
    })
 
+   $(document).on('click', 'a.section', function(event) {
+      event.preventDefault()
+      $("ul.lesson").remove()
+      let id = $(this).data("id")
+      let sectionChild = ""
+      $.get(`/sections/${id}.json`, function (section) {
+         const lessons = section["lessons"]
+         lessons.forEach(function(lesson) {
+            sectionChild += `
+               <li>
+                  <a href="/lessons/${lesson["id"]}">${lesson["name"]}</a>
+               </li>
+            `
+         })
+         $(`div.section#${id}`).append(
+            `<ul class="lesson">
+               <br>
+               <strong>Lessons</strong>
+               <br><br>
+               ${sectionChild}
+            </ul>`
+         )
+      })
+   })
 
 })
+
