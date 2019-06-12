69ac53d893595afa486c5f16fb65a060c53e9ccb
BebopVinh <vinhdo07@gmail.com>
2019-06-12 07:13:06 -0700

    Working section appending to category when link is clicked

--- app/assets/javascripts/categories.js
+++ app/assets/javascripts/categories.js
@@ -1,3 +1,21 @@
-$(function () {
-   console.log("ready!");
-});
+$(document).on('ready page:load', function () {
+   console.log("ready!")
+   $("a.category").click(function (event) {
+      event.preventDefault()
+      let id = $(this).data("id")
+      let categoryChild = ""
+      $.get(`/categories/${id}.json`, function (categories) {
+         const sections = categories["sections"].map(section => section)
+         sections.forEach(function (section) {
+            categoryChild += `
+               \<div class="section" id="${section["id"]}" data-id="${section["id"]}">
+                  <p>${section["name"]}</p>\
+               </div>\
+            `
+         })
+         $(`a.category#${id}`).append(`<ul>${categoryChild}</ul>`)
+      })
+   })
+
+
+})
