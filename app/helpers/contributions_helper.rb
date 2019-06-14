ea741d27e5bb755267c5e32410acd43ec59f68d4
BebopVinh <vinhdo07@gmail.com>
2019-06-13 20:15:14 -0700

    Update button text

--- app/helpers/contributions_helper.rb
+++ app/helpers/contributions_helper.rb
@@ -1,7 +1,7 @@
 module ContributionsHelper
    def update_buttons(contribution)
       if contribution.user == current_user
-         button_to "Edit Contribution", edit_contribution_path(contribution), method: :get, class: "pure-button pure-button-primary button-small"
+         button_to "Update Contribution", edit_contribution_path(contribution), method: :get, class: "pure-button pure-button-primary button-small"
       end
    end
 end
