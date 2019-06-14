4be82d1110aa80fd54750fc11ebb4635360d0427
BebopVinh <vinhdo07@gmail.com>
2019-06-13 20:17:03 -0700

    check user id and render content, buttons, and new contributions

--- app/assets/javascripts/lessons.js
+++ app/assets/javascripts/lessons.js
@@ -8,26 +8,66 @@ $(document).on('ready page:load', function () {
       }
    }
 
+   let currentUserId
+   $.get('/authenticate/user.json', function (token) {
+      currentUserId = token.id
+   })
+
+   function checkId(sourceId, currentUserId) {
+      if (sourceId === currentUserId) {
+         return true
+      } else {
+         return false
+      }
+   }
+
    Lesson.prototype.contributionContent = function () {
       const contributions = this.contributions
       let resultHTML = ""
-      debugger
       contributions.forEach(function (contribution) {
          resultHTML += `
          <section class="post">
             <header class="post-header">
                <h2 class="post-title">From ${contribution.user.username}</h2>
-            
             </header>
             <div class="post-description trix-content">
                ${contribution.content}
             </div>
-         </section>
          `
+         if (checkId(contribution.user.id, currentUserId)) {
+            resultHTML += `
+               <p>
+                  <a class="pure-button pure-button-primary button-small" href="/contributions/${contribution.id}/edit">Update Contribution</a>
+               </p></section>
+            `
+         } else {
+            resultHTML += `</section>`
+         }
       })
-      debugger
       return resultHTML
    }
+
+
+   $('form.new_contribution').submit(function (event) {
+      event.preventDefault()
+      const lessonId = parseInt($('input#lesson-id').val(), 10)
+      let values = $(this).serialize();
+      $.post(`/lessons/${lessonId}/contributions`, values)
+      .done(function(data) {
+         console.log(data)
+         let contributionHTML = `
+            <section class="post">
+               <header class="post-header">
+                  <h2 class="post-title">From ${data.user.username}</h2>
+               </header>
+               <div class="post-description trix-content">${data.content}</div>
+               <p>
+               <a class="pure-button pure-button-primary button-small" href="/contributions/${data.id}/edit">Update Contribution</a>
+               </p>
+            </section>`
+         $('div.lesson-contributions').append(contributionHTML)
+      })   
+   });
 
 
    $('a.lesson-browser').click(function(event) {
@@ -44,7 +84,12 @@ $(document).on('ready page:load', function () {
          $('input#lesson-id').val(currentLesson.id)
          let contributionHTML = currentLesson.contributionContent()
          $('div.lesson-contributions').html(contributionHTML)
-         debugger
+         if (checkId(currentLesson.id, currentUserId)) {
+            $('div#edit-button').html(
+               `<a href="/lessons/${currentLesson.id}/edit" class="pure-button button-warning">Edit this Lesson</a>`
+            )
+         }
+
       })
    })
 })
