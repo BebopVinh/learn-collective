3b91bb251fb8b440fb59cd2cceaddd1db5ef9df4
bebopvinh <vinhdo07@gmail.com>
2019-06-16 20:11:08 -0700

    Move Redcarpet markdown variable instantiation
    From application helper to contribution helper

--- app/helpers/contributions_helper.rb
+++ app/helpers/contributions_helper.rb
@@ -4,4 +4,28 @@ module ContributionsHelper
          button_to "Update Contribution", edit_contribution_path(contribution), method: :get, class: "pure-button pure-button-primary button-small"
       end
    end
+
+   def parse_content(content)
+      options = {
+      filter_html:     true,
+      hard_wrap:       true,
+      link_attributes: { rel: 'nofollow', target: "_blank" },
+      space_after_headers: true,
+      fenced_code_blocks: true
+      }
+
+      extensions = {
+      autolink: true,
+      genced_code_blocks: true,
+      strikethrough: true,
+      superscript: true,
+      underline: true,
+      quote: true,
+      disable_indented_code_blocks: false
+      }
+
+      renderer = Redcarpet::Render::HTML.new(options)
+      markdown = Redcarpet::Markdown.new(renderer, extensions)
+      markdown.render(content) unless (content == nil || content == "")
+   end
 end
